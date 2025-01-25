4591de8bdee62c9cc5475882f04fe110bef7164f
KinokoLu <myluwork004@gmail.com>
2025-01-25 13:21:47 -0800

    finished search bar

--- frontend/src/container/PropertyPage/PropertyPage.jsx
+++ frontend/src/container/PropertyPage/PropertyPage.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import "./PropertyPage.css"
 
 import { SearchBar } from "../../component";
+import { SearchResultList } from "../../component";
+
 
 const PropertyPage = () => {
+  
+  const [results, setResults] = useState([]);
+
+
   return (
     <div className="PropertyPage box__container">
       {/* <h1 className="title">PropertyPage</h1> */}
 
       <div className="feature-Container">
         <div className="searchBar-container">
-            <SearchBar/>
-            <div>SearchBarResult</div>
+            <SearchBar setResults = {setResults}/>
+            <SearchResultList results = {results}/>
         </div>
 
-        {/* <div className="FilterOption">
+        <div className="FilterOption">
             filter
         </div>
 
-        <div className="DisplayResult">
+        {/* <div className="DisplayResult">
             Result
         </div>     */}
 
