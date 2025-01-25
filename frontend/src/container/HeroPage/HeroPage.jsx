d8fcd694f5aca69158797336ae71760fc61b430b
KinokoLu <myluwork004@gmail.com>
2025-01-25 11:46:53 -0800

    update frontend Stuff

---
+++ frontend/src/container/HeroPage/HeroPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeroPage.css"
+
+
+const HeroPage = () => {
+  return (
+    <div className="HeroPage">
+      <span className="title">HeroPage</span>
+    </div>
+  );
+};
+
+export default HeroPage;
---
+++ frontend/src/container/index.js
@@ -0,0 +1,9 @@
+import PropertyPage from "./PropertyPage/PropertyPage";
+import Dashboard from "./DashBoard/DashBoard";
+import HeroPage from "./HeroPage/HeroPage";
+
+export  {
+    HeroPage,
+    PropertyPage,
+    Dashboard
+}
