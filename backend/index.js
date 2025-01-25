aeb10330c8e64c74c6e112358d28ec5cc4ea8b53
Foojanb <babaeeianwork@gmail.com>
2025-01-25 11:45:58 -0800

    sthh
    sth

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
-const express = require('express');
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
 const app = express();
 
 app.get('/', (req, res) => {
