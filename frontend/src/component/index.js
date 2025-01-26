90ea5cffd1b4e0b2a5f0547b4c19b46d16275a09
Foojanb <babaeeianwork@gmail.com>
2025-01-26 02:28:00 -0800

    changed and added quiz
    quiz

--- frontend/src/component/index.js
+++ frontend/src/component/index.js
@@ -2,8 +2,11 @@ import SearchBar from "./SearchBar/SearchBar";
 import SearchResultList from "./SearchBar/SearchResultList"
 import SearchResult from "./SearchBar/SearchResult"
 
-import NavBar from "./navBar/NavBar";
+import NavBar from "./navBar/NavBar"
 import AddressInput from "./Address/AddressInput"
+import Quiz from './QuizAnalyze/Quiz';
+
+
 
 export {
     SearchBar,
@@ -11,5 +14,6 @@ export {
     SearchResult,
 
     NavBar,
-    AddressInput
+    AddressInput, 
+    Quiz
 }
